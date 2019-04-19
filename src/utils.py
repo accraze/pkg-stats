4b727bbd4c9f629956790c0bcc978a4f28c55710
Andy Craze <accraze@gmail.com>
2019-04-19 10:01:28 -0700

    use top_n setting for print utils

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,16 @@
+from src.settings import TOP_N
+
+
 def _print_header(fname):
     bumper = ''.join(['*' for x in range(42)])
     print('\n' + bumper)
-    print('Top 10 Packages for {}'.format(fname))
+    print('Top {} Packages for {}'.format(TOP_N, fname))
     print(bumper)
+
 
 def _print_column_keys():
     print('   {:<20} \t {: <20}'.format('pkg_name', 'num_files'))
 
+
 def _print_row(idx, result):
     print('{}. {:<20} \t {: <20}'.format(idx+1, result[0], result[1]))
-
